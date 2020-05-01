792fc0cf0533ee7835a69e65c7306e400c8c632d
Frizzly <p.markus1337@gmail.com>
2020-05-01 18:16:16 +0200

    fix

--- pyth.py
+++ pyth.py
@@ -42,7 +42,7 @@ def load(file):
             return
 
     mapTerr = {'N': 200, 'H': 2}
-    mapTerr = collections.defaultdict(lambda:1, mapTerr)
+    mapTerr = collections.defaultdict(lambda: 1, mapTerr)
     princesses = []
     mapData = []
     start = 0
@@ -72,77 +72,84 @@ def findMinDist(npcData, princesses, dragon, start):
 
     for permutation in permutations(princesses):
         distance = npcData[start][dragon].dist     # distance to get to dragon
-        for start, dest in zip(permutation, (dragon,)+permutation):
-            distance += npcData[start][dest].dist
+        for begin, finish in zip((dragon,)+permutation, permutation):
+            distance += npcData[begin][finish].dist
         if distance < mini:
             mini, order = distance, permutation
 
-    return order
+    return order, mini
 
 def karp(npcData, princesses, dragon, start):
-    DIST, PATH = 0, 1
-    sets = [{frozenset(): (npcData[dragon][start].dist, start)}]
+    DIST, POS = 0, 1
+    sets = [{frozenset(): (npcData[start][dragon].dist, dragon)}]
 
     for row in range(1, len(princesses)+1):
-        sets.append({})
-        for subset in combinations(princesses, row):
-            for combo in sets[row-1]:
-                ssub = frozenset(subset)        # current combination
-                dest = list(ssub - combo)[0]        # 0 -> 1
-                start = sets[row-1][combo][PATH]
+        sets.append(collections.defaultdict(lambda:[maxsize]))
+        for comb in combinations(princesses, row):
+            for prevComb in sets[row-1]:
+                comb = frozenset(comb)
+                finish = list(comb - prevComb)[0]   # only one and first element can be there
+                begin = sets[row-1][prevComb][POS]
+                cost = sets[row-1][prevComb][DIST] + npcData[begin][finish].dist
+                if sets[row][comb][DIST] > cost:
+                    sets[row][comb] = cost, finish, begin
+    
+    paath = []
+    prev = comb
+    for i in range(row, 0, -1):
+        paath.append(sets[i][prev][POS])
+        prev = prev - {sets[i][prev][POS]}
 
-                if row == 1:                    # get normal matrix !
-                    dest, start = dragon, dest
+    return paath[::-1], sets[row][comb][DIST]
 
-                cost = sets[row-1][combo][DIST] + npcData[start][dest].dist
 
-                if sets[row].get(ssub, [maxsize])[DIST] > cost:
-                    sets[row][ssub] = cost, start
-        
-    return subset
-
-def getPath(npcData, start, dest, backward=False):
+def getPath(npcData, princesses, dragon, start, order):
     path = []
-    pos = npcData[dest][start].pos
-    if backward:
-        path.append(pos)
+    for star, dest in zip(order[::-1][1:]+(dragon,), order[::-1]):
+        path2 = []
+        pos = npcData[dest][star].pos
+        while pos != dest:
+            step = npcData[dest][pos].path
+            path2.append(step)
+            pos = npcData[dest][step].pos
 
-    while pos != dest:
-        step = npcData[dest][pos].path
-        path.append(step)
-        pos = npcData[dest][step].pos
+        path.append(path2)
+
+    path2 = []
+    pos = npcData[start][dragon].pos
+    path2.append(pos)
+
+    while pos != start:
+        step = npcData[start][pos].path
+        path2.append(step)
+        pos = npcData[start][step].pos
+
+    path.append(path2[::-1])
 
     return path
 
-def main():
-    mapData, princesses, dragon, start = load("mapa.txt") # load start too
-
-    npcData =       {p:         dijkstra(copy.deepcopy(mapData), p) for p in princesses}
-    npcData.update( {start:     dijkstra(copy.deepcopy(mapData), start)})
-    npcData.update( {dragon:    dijkstra(copy.deepcopy(mapData), dragon)})
-    
-    # we need the initial i node in order to get to j node (npcData[i][j]) by backtracking
-    # backtracking causes reversal path and distances. 
-    # in order to have consistent distances with all other npcs,
-    # we have to hackingly compute this node again with the help absolute value 
-
-    #for i in npcData[dragon]:
-    #    npcData[dragon][i.pos].dist = abs(i.dist - npcData[dragon][dragon].dist)
-
-    order = findMinDist(npcData, princesses, dragon, start)
-    order2 = karp(npcData, princesses, dragon, start)
-    
-    path = []
-    for star, dest in zip(order[::-1][1:]+(dragon,), order[::-1]):
-        path.append(getPath(npcData, star, dest))
-    path.append(getPath(npcData, dragon, start, True)[::-1])
-
+def printPath(path):
     for i, road in enumerate(path[::-1], 1):
         print(f"\n{i}: ", end=' ')
         for step in road:
             a = step // 5
             b = step % 5
             print(f"{a}{b}", end=' ')
+
+
+def main():
+    mapData, princesses, dragon, start = load("mapa.txt") # load start too
+
+    npcData = {p: dijkstra(copy.deepcopy(mapData), p) for p in princesses}
+    npcData.update({start: dijkstra(copy.deepcopy(mapData), start)})
+    npcData.update({dragon: dijkstra(copy.deepcopy(mapData), dragon)})
+
+    order, dist = findMinDist(npcData, princesses, dragon, start)
+    order2, dist2 = karp(npcData, princesses, dragon, start)
+    
+    path = getPath      (npcData, princesses, start, dragon, order)
+
+    printPath(path)
     
 main()
 
@@ -166,3 +173,12 @@ print(change)
 print(end)
 print(start)
 """
+
+
+# we need the initial i node in order to get to j node (npcData[i][j]) by backtracking
+# backtracking causes reversal path and distances. 
+# in order to have consistent distances with all other npcs,
+# we have to hackingly compute this node again with the help absolute value 
+
+#for i in npcData[dragon]:
+#    npcData[dragon][i.pos].dist = abs(i.dist - npcData[dragon][dragon].dist)
