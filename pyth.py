99ae8a8f497bb21af692cc011df817b8ba50f6fa
Frizzly <p.markus1337@gmail.com>
2020-05-02 18:35:13 +0200

    struggling

--- pyth.py
+++ pyth.py
@@ -65,7 +65,7 @@ def load(file):
         if (i+1) % maxCol != 0:
             mapData[i].edges.append(i+1)        # RIGHT
 
-    return mapData, princesses, dragon, start
+    return mapData, princesses, dragon, start, maxRow, maxCol
 
 def findMinDist(npcData, princesses, dragon, start):
     mini = maxsize
@@ -77,12 +77,30 @@ def findMinDist(npcData, princesses, dragon, start):
         if distance < mini:
             mini, order = distance, permutation
 
-    return order, mini
+    return (start, dragon) + order, mini
 
 def karp(npcData, princesses, dragon, start):
     DIST, POS = 0, 1
     sets = [{frozenset(): (npcData[start][dragon].dist, dragon)}]
+    princesses = frozenset(princesses)
+    '''
+    for row in range(1, len(princesses)+1):
+        sets.append(collections.defaultdict(lambda:[maxsize]))
+        for comb in combinations(princesses, row):
+            for prevComb in combinations(comb, len(comb)-1):
+                prevComb = frozenset(prevComb)
+                #prevComb = sets[row-1][frozenset(subcomb)]
+            
 
+            #for prevComb in sets[row-1]:
+                comb = frozenset(comb)
+                finish = list(comb - prevComb)[0]   # only one and first element can be there
+                begin = sets[row-1][prevComb][POS]
+                cost = sets[row-1][prevComb][DIST] + npcData[begin][finish].dist
+                if sets[row][comb][DIST] > cost:
+                    sets[row][comb] = cost, finish
+    '''
+    '''
     for row in range(1, len(princesses)+1):
         sets.append(collections.defaultdict(lambda:[maxsize]))
         for comb in combinations(princesses, row):
@@ -92,53 +110,122 @@ def karp(npcData, princesses, dragon, start):
                 begin = sets[row-1][prevComb][POS]
                 cost = sets[row-1][prevComb][DIST] + npcData[begin][finish].dist
                 if sets[row][comb][DIST] > cost:
-                    sets[row][comb] = cost, finish, begin
-    
+                    sets[row][comb] = cost, finish
+    '''
+    sets = [{frozenset(): (npcData[start][dragon].dist, dragon)}]
+    #nmap = [{frozenset(): {dragon: (npcData[start][dragon].dist, start)}}]
+    nmap = []
+    # [rowLIST][setDICT][finish]
+
+    # nakreslit si algoritmus (grafy), zapisovat si vypocitavania (matika), 
+    # spojit dokopy a hladat vztahy
+
+    # rowLIST = cislo riadku
+    # setDICT = dictionary, pri ktorom key je frozenset kombinacie (groupa v ramci riadku)
+    # finish =  dictionary, pri ktorom key je finish, a value je jeho cost a predosli node (node)
+    princesses = frozenset(princesses)
+
+    for row in range(len(princesses)):
+        nmap.append({})
+        for comb in combinations(princesses, row):          # prava strana combo
+            comb2 = frozenset(comb)
+            nmap[row].update({comb2: {}})
+
+            for finish in princesses - comb2:                # lava strana
+                for subcomb in combinations(comb, row-1):
+                    subcomb2 = frozenset(subcomb)
+                    begin = nmap[row-1][subcomb2][2]
+                    prevCost = nmap[row-1][subcomb2]
+                # comb je zaciatok
+                # dostanem ale 24,20,4
+                # potrebujem tie ktore su nadtym, tj 24,20; 24,4; 20,4
+                '''
+                for prevComb in prevCombs:
+                    prevComb2 = frozenset(prevComb)
+                for prevComb in nmap[row-1]:
+                    if prevComb[princesses - comb2 - {finish}] in nmap[row-1]
+                '''
+                # ideme do nodu FINISH, ako zistit begin? v combo mame begin - to je ale mnozina
+                # takze musime ju zminusovat s niecim takze list(comb2 - ...)
+                # zminusovat ju musime z predosleho rowu.. ako? kde a jak
+                # v prvom pripade to je comb2 - {}
+                # v druhom pripade to bude tiez comb2 - {} bo v minulom sme este nemali nic v mnozine
+                # ist do frozensetov ktore nemaju finish
+
+                # spravit kombinacie s poctom o -1
+
+                if row:
+                    begin = list(comb2)[0]
+                    # comb3 = comb2.union()
+                    prevCost = nmap[row-1][prevComb][begin][DIST]       # PRESUNUT HORE
+                else:
+                    prevCost, begin = npcData[start][dragon].dist, dragon
+                cost = npcData[begin][finish].dist + prevCost
+
+                nmap[row][comb2].update({finish: (cost, begin, finish)})
+        #prevCombs = combs
+
+
+
+
+        '''
+        for prevComb in sets[row-1]:                    # 
+            prevComb = frozenset(prevComb)
+            for finish in comb2 - prevComb:
+                begin = sets[row-1][prevComb][POS]
+                cost = sets[row-1][prevComb][DIST] + npcData[begin][finish].dist
+                if sets[row][comb2][DIST] > cost:
+                    sets[row][comb2] = cost, finish
+                a=1
+                b=2
+            a=1
+            b=2
+        '''
+        '''
+        for compart in combinations(comb, len(comb)-1): 
+            sets[row-1][compart] - compart
+
+        for prevComb in sets[row-1]:
+
+            comb = frozenset(comb)
+            finish = list(comb - prevComb)[0]   # only one and first element can be there
+            begin = sets[row-1][prevComb][POS]
+            cost = sets[row-1][prevComb][DIST] + npcData[begin][finish].dist
+            if sets[row][comb][DIST] > cost:
+                sets[row][comb] = cost, finish
+        '''
+
     paath = []
     prev = comb
     for i in range(row, 0, -1):
         paath.append(sets[i][prev][POS])
         prev = prev - {sets[i][prev][POS]}
 
-    return paath[::-1], sets[row][comb][DIST]
+    return [start, dragon] + paath[::-1], sets[row][comb][DIST]
 
-
-def getPath(npcData, princesses, dragon, start, order):
+def getPath(npcData, order):
     path = []
-    for star, dest in zip(order[::-1][1:]+(dragon,), order[::-1]):
+
+    for begin, finish in zip(order, order[1:]):
         path2 = []
-        pos = npcData[dest][star].pos
-        while pos != dest:
-            step = npcData[dest][pos].path
-            path2.append(step)
-            pos = npcData[dest][step].pos
-
-        path.append(path2)
-
-    path2 = []
-    pos = npcData[start][dragon].pos
-    path2.append(pos)
-
-    while pos != start:
-        step = npcData[start][pos].path
-        path2.append(step)
-        pos = npcData[start][step].pos
-
-    path.append(path2[::-1])
+        while finish != begin:
+            path2.append(finish)
+            finish = npcData[begin][finish].path
+        path.append(path2[::-1])
 
     return path
 
-def printPath(path):
-    for i, road in enumerate(path[::-1], 1):
+def printSolution(path, maxRow, maxCol):
+    for i, road in enumerate(path, 1):
         print(f"\n{i}: ", end=' ')
         for step in road:
-            a = step // 5
-            b = step % 5
+            a = step // maxRow
+            b = step % maxCol
             print(f"{a}{b}", end=' ')
 
 
 def main():
-    mapData, princesses, dragon, start = load("mapa.txt") # load start too
+    mapData, princesses, dragon, start, maxRow, maxCol = load("mapa2.txt")
 
     npcData = {p: dijkstra(copy.deepcopy(mapData), p) for p in princesses}
     npcData.update({start: dijkstra(copy.deepcopy(mapData), start)})
@@ -147,38 +234,13 @@ def main():
     order, dist = findMinDist(npcData, princesses, dragon, start)
     order2, dist2 = karp(npcData, princesses, dragon, start)
     
-    path = getPath      (npcData, princesses, start, dragon, order)
+    path = getPath(npcData, order)
+    path2 = getPath(npcData, order2)
 
-    printPath(path)
+    print(dist, end='\n')
+    printSolution(path, maxRow, maxCol)
+    print(dist2, end='\n')
+    printSolution(path2, maxRow, maxCol)
+    a=2
     
 main()
-
-
-
-"""
-# from heapq import heappush, heappop
-
-end = ["1","2","3","4","5","6","7","8","0"]
-start = ["0","1","2","3","4","5","6","7","8"]
-start_text = ("Starting state:\n" + " ".join(start) +
-            "\nEnter 9 numbers if you want to have custom starting state\n")
-end_text = ("Ending state:\n" + " ".join(end) + 
-            "\nEnter another 9 numbers if you want to have custom ending state")
-
-change = input(start_text)
-end = end if change else input(end_text).split()
-start = start if change else change.split()
-
-print(change)
-print(end)
-print(start)
-"""
-
-
-# we need the initial i node in order to get to j node (npcData[i][j]) by backtracking
-# backtracking causes reversal path and distances. 
-# in order to have consistent distances with all other npcs,
-# we have to hackingly compute this node again with the help absolute value 
-
-#for i in npcData[dragon]:
-#    npcData[dragon][i.pos].dist = abs(i.dist - npcData[dragon][dragon].dist)
