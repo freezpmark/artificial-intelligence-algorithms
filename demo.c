bc5746fd98b7af15758fb36e1bc6b6b70a52b8a9
Frizzly <p.markus1337@gmail.com>
2020-05-31 15:39:25 +0100

    Remove unecessary files

--- demo.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-void main() {
-    printf("Hello World!");
-    printf("Hello World!");
-    printf("Hello World!");
-    printf("Hello World!");
-    printf("Hello World!");
-}
