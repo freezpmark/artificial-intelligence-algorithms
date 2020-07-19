083b0c087922c3576cb6fa8399b9088ff183e6ea
Frizzly <p.markus1337@gmail.com>
2020-07-19 14:28:05 +0100

    Add .vscode settings

--- setup.py
+++ setup.py
@@ -2,4 +2,5 @@ from setuptools import find_packages, setup
 
 with open("reqs.txt") as f:
     reqs = f.read().splitlines()
+
 setup(name="simulation", install_requires=reqs, packages=find_packages())
