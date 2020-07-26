a4c038b6b4745951ab84a72358b6cce716fbedd4
Frizzly <p.markus1337@gmail.com>
2020-07-26 10:08:44 +0100

    Attempt - get actions from rules without recursion

--- simulation/production_system.py
+++ simulation/production_system.py
@@ -1,105 +1,59 @@
 import collections as col
 from itertools import islice
 
-# USE REGEXP TO VALIDATE TEXT FILES
-# while True:
-#     line = f1.readline()
-#     if line.endswith(":"):
-#         name = line
-#         line = f1.readline()
-#         if line.startswith("AK"):
-#             cond = line
-#         line = f1.readline()
-#         if line.startswith("POTOM"):
-#             act = line
-
-#     name = f1.readline()
-#     cond = f1.readline()
-#     act = f1.readline()
-
-#     rule = islice(infile, N)
-
-# f1.readline()
-# for i, line in enumerate(f1, 1):
-#     if line.endswith(":"):
-#         name = line
-#     elif line.startswith("AK"):
-#         cond = line
-#     elif line.startswith("POTOM"):
-#         act = line
-#     elif not line:
-#         if name and cond and act:
-#             rules.append(Rules(name, cond, act))
-#         else:
-#             print("WRONG!")
-#         # create rule
-# rule = []
-# for i, line in enumerate(f1, 1):
-#     rule.append(line.rstrip())
-#     if i % 3 == 0:
-#         rules.append(Rules(*rule))
-#         rule = []
-
-# a=1
-
-# while f1:
-#     rules.append(Rule(*[line for line in f1][:3]))
-
-# for rule in f1[:2:3]:
-# for f1[::3]
-
-# lines_gen = islice(f1, 3)
-# lines = [line.rstrip() for line in lines_gen]
-
-# line = f2.readline().rstrip()
-# rules = []
-# for i, line in enumerate(f1):
-#     if i % 3 == 0:
-#         pass  # append
-
-# facts = []
-# for line in f2:
-#     line = line.rstrip()
-#     # append..
-
-# rule = [
-#     part[1] if part[0].isupper() else part[0][:-1]
-#     for line in islice(f1, 3)
-#     for part in line.split(' \n', 1)
-# ]
-
 
 def runProduction():
-    Rules = col.namedtuple("Rule", "name cond act")
+    Rules = col.namedtuple("Rule", "name conds acts")
 
     # ToDo: check whether file is in correct format with regexp
-    with open("simulation/rules2.txt") as f1:
+    # we've loaded the files
+    with open("simulation/rules.txt") as f1:
         rules = []
         while rule := [line.rstrip("\n:") for line in islice(f1, 3)]:
             rules.append(Rules(*rule))
             f1.readline()
-
     with open("simulation/facts.txt") as f2:
         facts = [fact.rstrip() for fact in f2]
 
-    # we've loaded the files
-    print(rules)
-    print(facts)
+    while True:
+        for rule in rules:
+            mapping = {}
+            conds = rule.conds.split(",")
+            for cond in conds:
+                for fact in facts:
+                    pot_mapping = {}
+                    upd = True
+                    csplit = cond.split()
+                    fsplit = fact.split()
+                    for i, (c, f) in enumerate(zip(csplit, fsplit)):
+                        if c[0] == "?" and c not in mapping:
+                            pot_mapping[c] = f
+                        elif c != f:
+                            upd = False
+                            break
+                        elif c[0] == "<":
+                            if mapping[csplit[i+1]] == mapping[csplit[i+2]]:
+                                upd = False
+                            break
+                    if upd:
+                        mapping = {**mapping, **pot_mapping}
+                        break
 
-    # iterate through facts that match current rule (strip names and then use in)
+            print(mapping)
+        break
 
 
 if __name__ == "__main__":
     runProduction()
 
-    # save rules: rule -> rule -> rule -> rule: struct(name, cond, action) 3 sentences
-    # save facts: fact -> fact -> fact -> fact: struct(<cond>) 1 sentence
+    # save rules: rule -> rule -> rule -> rule: struct(name, cond, action)
+    # save facts: fact -> fact -> fact -> fact: struct(<cond>)
 
     # ITERATE through to-be FACTS
     #     ITERATE through RULES
     #         ITERATE through RULES' BRACKETS (recursively)
     #             ITERATE through FACTS to fill ?X's (everywhere) and ?Y etc.
-    #         Save action to be processed (actions are reseted after each action)
+    #         Save action to be processed (actions are reseted after action)
 
     # () with <> means they have to be different
     # (they can be the same among siblings! Parent can have 2x the same child)
