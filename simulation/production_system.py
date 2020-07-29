5a1aa4dde92c1caaa70390262c567aba04a034f0
Frizzly <p.markus1337@gmail.com>
2020-07-29 20:57:46 +0100

    Update - optimize code and namings

--- simulation/production_system.py
+++ simulation/production_system.py
@@ -2,62 +2,55 @@ import collections as col
 from itertools import islice
 
 
-def expand(conds, facts, mapping):
-    # ToDo: beautify this function!
+def expand(conds, facts, label):
     # LOOP over rule's conditions recursively
 
-    acts = []
     # LOOP over facts
+    labels = []
     for fact in facts:
-        fact = fact.split()  # uz sme daco appendli for act!
-        tmp_mapping = {}
-        upd = True
+        fact = fact.split()
+        tmp_label = {}
+        continue_ = True
         for i, (c, f) in enumerate(zip(conds, fact)):
             c_key = c.rstrip(",")
+            # label checking for "?"
             if c_key.startswith("?") and f[0].isupper():  # new entity
-                if c_key not in mapping:
-                    if f not in mapping.values():
-                        tmp_mapping[c_key] = f  # # neexistuje ani jedno
+                if c_key not in label:
+                    if f not in label.values():
+                        tmp_label[c_key] = f
                     else:
-                        upd = False
-                        break  # f existuje, a c neexistuje
-                elif mapping[c_key] != f:
-                    upd = False
-                    break  # c existuje, f v nom nieje -> break
-                else:
-                    continue  # ak existuju oba, continue
-            # () with <> means they have to be different
-            elif c_key.startswith("<"):  # special cond at the end,,
-                if mapping[conds[i + 1]] == mapping[conds[i + 2]]:
-                    upd = False
-                break
-            elif c_key != f:  # difference
-                upd = False
+                        continue_ = False
+                elif label[c_key] != f:
+                    continue_ = False
+            # key identity checking with <> special cond
+            elif c_key.startswith("<"):
+                if label[conds[i + 1]] == label[conds[i + 2]]:
+                    continue_ = False
+            # unmatched condition with fact
+            elif c_key != f:
+                continue_ = False
+
+            if not continue_:
                 break
 
-            if c.endswith(","):  # next cond -> recursive call
-                new_act = expand(
-                    conds[i + 1 :], facts, {**mapping, **tmp_mapping}
-                )
-                if new_act:
-                    if isinstance(new_act, list):  # nesting conds
-                        acts += new_act
-                    else:
-                        acts.append(new_act)
+            # next condition -> recursive call
+            if c.endswith(","):
+                labels += expand(conds[i + 1 :], facts, {**label, **tmp_label})
 
-        if upd and not c.endswith(","):  # found a match for action!
-            # return {**mapping, **tmp_mapping}
-            acts.append({**mapping, **tmp_mapping})  # save action
-            if c == "<>":  # facts are irelevant, we would do duplicates
+        # label match found for action
+        if continue_ and not c.endswith(","):
+            labels.append({**label, **tmp_label})
+            if c == "<>":  # iterating facts are irelevant as its independent
                 break
 
-    return acts
+    return labels
 
 
 def loadFiles():
-    Rules = col.namedtuple("Rule", "name conds acts")
+
+    Rules = col.namedtuple("Rule", "name conds labels")
+    rules = []
     with open("simulation/rules.txt") as f1:
-        rules = []
         while rule := [line.rstrip("\n:") for line in islice(f1, 3)]:
             rules.append(Rules(*rule))
             f1.readline()
@@ -72,16 +65,16 @@ def findActions(rules, facts):
 
     actions_found = []
     for rule in rules:  # LOOP over rules
-        rule_acts = [act.split(" ", 1) for act in rule.acts.split(", ")]
+        rule_acts = [act.split(" ", 1) for act in rule.labels.split(", ")]
         rule_acts_label = expand(rule.conds.split(), facts, {})
         for label in rule_acts_label:
 
             actions = []
-            for act_type, action in rule_acts:
+            for type_, action in rule_acts:
                 for key in [key.rstrip(",") for key in action.split()]:
                     if key.startswith("?"):
                         action = action.replace(key, label[key])
-                actions.append(act_type + " " + action)
+                actions.append(type_ + " " + action)
 
             actions_found.append(actions)
 
@@ -92,14 +85,14 @@ def removeDuplicates(actions_found, facts):
 
     i = 0
     for _ in range(len(actions_found)):
-        message = True  # happens when there wasnt a duplicate in prev acts
+        message = True  # happens when there wasnt a duplicate in prev labels
         j = 0
         for _ in range(len(actions_found[i])):
-            act_type, act = actions_found[i][j].split(" ", 1)
+            type_, act = actions_found[i][j].split(" ", 1)
             if (
-                (act_type == "pridaj" and act in facts)
-                or (act_type == "vymaz" and act not in facts)
-                or (act_type == "sprava" and not message)
+                (type_ == "pridaj" and act in facts)
+                or (type_ == "vymaz" and act not in facts)
+                or (type_ == "sprava" and not message)
             ):
                 del actions_found[i][j]
                 message = False
@@ -117,12 +110,12 @@ def applyActions(actions_appliable, facts):
 
     messages = []
     for action in actions_appliable[0]:
-        act_type, act = action.split(" ", 1)
-        if act_type == "pridaj":
+        type_, act = action.split(" ", 1)
+        if type_ == "pridaj":
             facts.append(act)
-        elif act_type == "vymaz":
+        elif type_ == "vymaz":
             facts.remove(act)
-        elif act_type == "sprava":
+        elif type_ == "sprava":
             messages.append(act)
 
     return facts, messages
